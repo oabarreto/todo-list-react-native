c4247a586eab0d0ab9af17712170f2b6b06c1e45
Otávio Barreto <oabarreto@ichurch.com.br>
2024-09-06 19:19:21 -0300

    add NativeWind

---
+++ src/styles/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  base: {
+    gray: {
+      700: "#0D0D0D",
+      600: "#1A1A1A",
+      500: "#262626",
+      400: "#333333",
+      300: "#808080",
+      200: "#D9D9D9",
+      100: "#F2F2F2",
+    },
+  },
+  product: {
+    blue_dark: "#1E6F9F",
+    blue: "#4EA8DE",
+    purple_dark: "#5E60CE",
+    purple: "#8284FA",
+  },
+  feedback: {
+    danger: "#E25858",
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import { colors } from "./src/styles/colors";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors,
+    },
+  },
+  plugins: [],
+};
